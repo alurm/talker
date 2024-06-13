9b2bd1d86ec75fd1715490c274bd999cec0ac659
Alan Urmancheev <alan.urman@gmail.com>
2024-06-13 06:50:14 +0400

    Replace talker with a different implementation

--- talker.c
+++ talker.c
@@ -1,127 +1,59 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
-#include <sys/select.h>
+#include <stdlib.h>
+#include <netdb.h>
 #include <stdarg.h>
+#include <stdio.h>
 
-#ifdef debugging
-#include <assert.h>
-#endif
-
-int decimal_size(int i) {
-	int size = 1;
-	while (i != 0) {
-		i /= 10;
-		size++;
-	}
-	return size;
+static void die(void) {
+	char message[] = "Fatal error\n";
+	write(2, message, sizeof message - 1);
+	exit(1);
 }
 
-int formatted_message_size(char *format, va_list arguments) {
-	int size = 0;
-	for (int i = 0; format[i]; i++) {
-		switch (format[i]) {
-		case '%': {
-			switch (format[i + 1]) {
-			case 's': {
-				size += strlen(va_arg(arguments, char *));
-				i++;
-				break;
-			}
-			case 'd': {
-				size += decimal_size(va_arg(arguments, int));
-				i++;
-				break;
-			}
-			}
-			break;
-		}
-		default:
-			size++;
-			break;
-		}
-	}
-	return size;
-}
+typedef struct {
+	char *data;
+	int size;
+} string;
 
-void *try_malloc(int size) {
-	void *result = malloc(size);
-	if (result == 0) {
-		write(2, "Fatal error\n", sizeof "Fatal error\n" - 1);
-		exit(1);
-	}
-	return result;
-}
-
-static char *format_message_given_arguments_list(char *format, va_list arguments) {
-	va_list copy;
-	va_copy(copy, arguments);
-	int size = formatted_message_size(format, copy);
-	char *message = try_malloc(size + 1);
-	vsprintf(message, format, arguments);
-	va_end(copy);
-	return message;
-}
-
-static char *format_message(char *format, ...) {
-	va_list arguments;
-	va_start(arguments, format);
-	char *result = format_message_given_arguments_list(format, arguments);
-	va_end(arguments);
-	return result;
-}
-
-static ssize_t try(int i){
-	if (i == -1){
-#ifdef debugging
-		char *message = format_message("Fatal error: %s\n", strerror(errno));
-		int message_size = strlen(message);
-		write(2, message, message_size);
-		free(message);
-#else
-		write(2, "Fatal error\n", sizeof "Fatal error\n" - 1);
-#endif
-		exit(1);
-	}
-	return i;
-}
-
-static int try_in_debug(int i) {
-#ifdef debugging
-	if (i == -1){
-		char *message = format_message("debug: error: %s\n", strerror(errno));
-		int message_size = strlen(message);
-		write(2, message, message_size);
-		free(message);
-		exit(1);
-	}
-#endif
-	return i;
-}
+typedef struct {
+	string string;
+	int written;
+} pending_write;
 
 typedef struct list {
-	void *data;
+	union {
+		char character;
+		pending_write pending_write;
+	};
 	struct list *next;
 } list;
 
-list *make_list(void *data) {
-	list *l = try_malloc(sizeof(list));
-	l->data = data;
-	l->next = 0;
+static void *allocate(int size) {
+	void *result = malloc(size);
+	if (result == 0)
+		die();
+	return result;
+}
+
+list *new_pending_write_list(string s) {
+	list *l = allocate(sizeof(list));
+	*l = (list) { .pending_write = (pending_write) { .string = s } };
 	return l;
 }
 
-typedef struct queue {
+list *new_character_list(char c) {
+	list *l = allocate(sizeof(list));
+	*l = (list) { .character = c };
+	return l;
+}
+
+typedef struct {
 	list *front;
 	list *back;
 } queue;
 
-void enqueue(queue *q, list *l) {
+static void enqueue(queue *q, list *l) {
 	if (q->back)
 		q->back->next = l;
 	else
@@ -129,7 +61,7 @@ void enqueue(queue *q, list *l) {
 	q->back = l;
 }
 
-list *dequeue(queue *q) {
+static list *dequeue(queue *q) {
 	list *result = q->front;
 	q->front = q->front->next;
 	if (q->front == 0)
@@ -137,193 +69,211 @@ list *dequeue(queue *q) {
 	return result;
 }
 
-static fd_set reads[1], readees[1], writes[1], writees[1], errors[1], errorees[1];
-static int number, listener, i;
-
-typedef struct {
-	char *data;
-	int size;
-	int written;
-} line;
-
-line *make_line(char *message, int size) {
-	line *l = try_malloc(sizeof(line));
-	*l = (line) {
-		.data = message,
-		.size = size,
-	};
-	return l;
-}
-
-char *make_char(char c) {
-	char *result = try_malloc(1);
-	*result = c;
-	return result;
-}
-
 static struct client {
-	queue chars_in;
-	queue lines_out;
 	_Bool live;
 	int number;
-} clients[FD_SETSIZE], *c;
+	queue characters;
+	queue pending_writes;
+} clients[FD_SETSIZE], *client;
 
-char *clone(char *string, int size) {
-	char *result = try_malloc(size + 1);
-	result[size] = 0;
-	for (int i = 0; i < size; i++)
-		result[i] = string[i];
+static string clone(string s) {
+	char *data = allocate(s.size + 1);
+	data[s.size] = 0;
+	for (int i = 0; i < s.size; i++)
+		data[i] = s.data[i];
+	return (string) {
+		.data = data,
+		.size = s.size,
+	};
+}
+
+static int size(char *s) {
+	int result = 0;
+	for (;;) {
+		if (s[result] == 0)
+			return result;
+		result++;
+	}
+}
+
+int get_formatted_int_size(int i) {
+	int result = 1;
+	while (i != 0) {
+		i /= 10;
+		result++;
+	}
 	return result;
 }
+
+static int get_formatted_size(char *format, va_list arguments) {
+	int result = 0;
+	for (int i = 0; format[i]; i++)
+		switch (format[i]) {
+			default:
+				result++;
+				break;
+			case '%':
+				switch (format[i + 1]) {
+					case 'd':
+						result += get_formatted_int_size(va_arg(arguments, int));
+						i++;
+						break;
+					case 's':
+						result += size(va_arg(arguments, char *));
+						i++;
+						break;
+					default:
+						die();
+				}
+		}
+	return result;
+}
+
+static string do_format(char *format, va_list arguments) {
+	va_list copy;
+	va_copy(copy, arguments);
+	int size = get_formatted_size(format, copy);
+	char *data = allocate(size + 1);
+	vsprintf(data, format, arguments);
+	printf("formatted: %s", data);
+	va_end(copy);
+	return (string) {
+		.data = data,
+		.size = size,
+	};
+}
+
+static int listening_socket, total_visitors, i;
 
 static void notify(char *format, ...) {
 	va_list arguments;
 	va_start(arguments, format);
-	char *message = format_message_given_arguments_list(format, arguments);
-	int message_size = strlen(message);
+	string s = do_format(format, arguments);
 	for (int j = 0; j < FD_SETSIZE; j++)
-		if (j != i && j != listener && clients[j].live) {
+		if (j != listening_socket && j != i && clients[j].live) {
 			enqueue(
-				&clients[j].lines_out,
-				make_list(make_line(clone(message, message_size), message_size))
+				&clients[j].pending_writes,
+				new_pending_write_list(clone(s))
 			);
 		}
-	free(message);
 	va_end(arguments);
+	free(s.data);
 }
 
-static void client(void) {
-	if (FD_ISSET(i, reads)) {
-		char buffer[(2 << 10) + 1];
-		int size = try_in_debug(recv(i, buffer, sizeof buffer - 1, 0));
-		if (size == 0) {
-			try_in_debug(close(i));
-			FD_CLR(i, readees);
-			FD_CLR(i, writees);
-			FD_CLR(i, errorees);
-			c->live = 0;
-			notify("server: client %d left\n", c->number);
-			{
-				queue *q = &c->chars_in;
-				while (q->front) {
-					free(q->front->data);
-					free(dequeue(q));
-				}
-			}
-			{
-				queue *q = &c->lines_out;
-				while (q->front) {
-					line *l = (line *)q->front->data;
-					free(l->data);
-					free(l);
-					free(dequeue(q));
-				}
-			}
-		}
-		else
-			for (int i = 0; i < size; i++) {
-				enqueue(&c->chars_in, make_list(make_char(buffer[i])));
-				if (buffer[i] == '\n') {
-					queue *q = &c->chars_in;
+static fd_set may_need_to_read_from, may_need_to_write_to, can_read_from, can_write_to;
+
+static void do_listening_socket(void) {
+	if (FD_ISSET(i, &can_read_from)) {
+		int new = accept(
+			i,
+			(struct sockaddr *)&(struct sockaddr_storage) { 0 },
+			&(socklen_t) { sizeof(struct sockaddr_storage) }
+		);
+		if (new == -1)
+			return;
+		client = clients + new;
+		*client = (struct client) {
+			.live = 1,
+			.number = total_visitors,
+		};
+		total_visitors++;
+		i = new;
+		notify("server: client %d joined\n", client->number);
+		FD_SET(new, &may_need_to_read_from);
+		FD_SET(new, &may_need_to_write_to);
+	}
+}
+
+static void do_client_socket(void) {
+	if (FD_ISSET(i, &can_read_from)) {
+		char buffer[2 << 10];
+		int read = recv(i, buffer, sizeof buffer, 0);
+		if (read == -1)
+			return;
+		if (read == 0) {
+			client->live = 0;
+			close(i);
+			notify("server: client %d left\n", client->number);
+			FD_CLR(i, &may_need_to_read_from);
+			FD_CLR(i, &may_need_to_write_to);
+			for (queue *q = &client->pending_writes; q->front; free(dequeue(q)))
+				free(q->front->pending_write.string.data);
+			for (queue *q = &client->characters; q->front; free(dequeue(q)))
+				;
+		} else {
+			queue *q = &client->characters;
+			for (int r = 0; r < read; r++) {
+				enqueue(q, new_character_list(buffer[r]));
+				if (buffer[r] == '\n') {
 					int size = 0;
 					for (list *l = q->front; l; l = l->next)
 						size++;
-					char *line = try_malloc(size + 1);
-					line[size] = 0;
-					for (int i = 0; i < size; i++) {
+					char *data = allocate(size + 1);
+					data[size] = 0;
+					for (int s = 0; s < size; s++) {
 						list *l = dequeue(q);
-						char *ch = l->data;
-						line[i] = *ch;
-						free(l->data);
+						data[s] = l->character;
 						free(l);
 					}
-					notify("client %d: %s", c->number, line);
-					free(line);
+					printf("data: %s", data);
+					notify("client %d: %s", client->number, data);
+					free(data);
 				}
-			}
-	}
-	if (FD_ISSET(i, writes)) {
-		list *front = c->lines_out.front;
-		if (front) {
-			line *l = front->data;
-			int sent = try_in_debug(send(i, l->data + l->written, l->size - l->written, 0));
-			l->written += sent;
-			if (l->written == l->size) {
-				free(l->data);
-				free(l);
-				free(dequeue(&c->lines_out));
 			}
 		}
 	}
-	if (FD_ISSET(i, errors))
-#ifdef debugging
-		assert(0)
-#endif
-		;
-}
-
-static void server(void) {
-	if (FD_ISSET(i, reads)) {
-		i = try_in_debug(
-			accept(
-				listener,
-				(struct sockaddr *)&(struct sockaddr_in){ 0 },
-				&(socklen_t){ sizeof(struct sockaddr_in) }
-			)
-		);
-		FD_SET(i, readees);
-		FD_SET(i, writees);
-		FD_SET(i, errorees);
-		c = clients + i;
-		c->live = 1;
-		c->number = number;
-		number++;
-		notify("server: client %d joined\n", c->number);
-	}
-	if (FD_ISSET(i, writes))
-#ifdef debugging
-		assert(0)
-#endif
-		;
-	if (FD_ISSET(i, errors))
-#ifdef debugging
-		assert(0)
-#endif
-		;
-}
-
-static void events(void) {
-	*reads = *readees;
-	*writes = *writees;
-	*errors = *errorees;
-	try_in_debug(select(FD_SETSIZE, reads, writes, errors, 0));
-	for (i = 0; i < FD_SETSIZE; i++) {
-		c = clients + i;
-		if (i == listener)
-			server();
-		else
-			client();
+	if (FD_ISSET(i, &can_write_to)) {
+		queue *q = &client->pending_writes;
+		if (q->front) {
+			pending_write *pw = &q->front->pending_write;
+			int written = send(
+				i, pw->string.data + pw->written, pw->string.size - pw->written, 0
+			);
+			if (written == -1)
+				return;
+			pw->written += written;
+			if (pw->written == pw->string.size) {
+				free(pw->string.data);
+				free(dequeue(q));
+			}
+		}
 	}
 }
 
-int main(){
-	listener = try(socket(AF_INET, SOCK_STREAM, 0));
+static void do_select(void) {
+	can_read_from = may_need_to_read_from;
+	can_write_to = may_need_to_write_to;
+	select(FD_SETSIZE, &can_read_from, &can_write_to, 0, 0);
+	for (i = 0; i < FD_SETSIZE; i++)
+		if (i == listening_socket)
+			do_listening_socket();
+		else {
+			client = clients + i;
+			do_client_socket();
+		}
+}
+
+int main(void) {
+	listening_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening_socket == -1)
+		die();
 #ifdef debugging
-	try(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)));
+	if (setsockopt(listening_socket, SOL_SOCKET, SO_REUSEADDR, &(int) { 1 }, sizeof(int)) == -1)
+		die();
 #endif
-	try(bind(
-		listener,
-		(struct sockaddr *)&(struct sockaddr_in){
+	if (bind(
+		listening_socket,
+		(struct sockaddr *)&(struct sockaddr_in) {
 			.sin_family = AF_INET,
-			.sin_port = htons(8000),
 			.sin_addr = htonl((127 << 24) + 0 + 0 + 1),
+			.sin_port = htons(8000),
 		},
 		sizeof(struct sockaddr_in)
-	));
-	try(listen(listener, SOMAXCONN));
-	FD_SET(listener, readees);
-	FD_SET(listener, writees);
-	FD_SET(listener, errorees);
+	) == -1)
+		die();
+	if (listen(listening_socket, SOMAXCONN) == -1)
+		die();
+	FD_SET(listening_socket, &may_need_to_read_from);
 	for (;;)
-		events();
+		do_select();
 }
